998471ef94ed4a6ccd99ed9c110d2caa947d389b
Александр Ольшанский <alex.olshanskii2006@gmail.com>
2024-11-22 17:27:52 +0500

    Добавление связи с БД, используя метод EnsureCreated, вывод списка пользователей

---
+++ Lb1/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lb1
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=Lemon4ik62212006");
+        }
+    }
+}
---
+++ Lb1/FormMain.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Lb1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelTop = new Panel();
+            labelUsers = new Label();
+            panelBot = new Panel();
+            labelInfAboutUsers = new Label();
+            panelTop.SuspendLayout();
+            panelBot.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelTop
+            // 
+            panelTop.Controls.Add(labelUsers);
+            panelTop.Dock = DockStyle.Top;
+            panelTop.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            panelTop.Location = new Point(0, 0);
+            panelTop.Name = "panelTop";
+            panelTop.Padding = new Padding(10);
+            panelTop.Size = new Size(684, 50);
+            panelTop.TabIndex = 0;
+            // 
+            // labelUsers
+            // 
+            labelUsers.Dock = DockStyle.Fill;
+            labelUsers.Location = new Point(10, 10);
+            labelUsers.Name = "labelUsers";
+            labelUsers.Size = new Size(664, 30);
+            labelUsers.TabIndex = 0;
+            labelUsers.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // panelBot
+            // 
+            panelBot.Controls.Add(labelInfAboutUsers);
+            panelBot.Dock = DockStyle.Fill;
+            panelBot.Location = new Point(0, 50);
+            panelBot.Name = "panelBot";
+            panelBot.Padding = new Padding(10);
+            panelBot.Size = new Size(684, 511);
+            panelBot.TabIndex = 1;
+            // 
+            // labelInfAboutUsers
+            // 
+            labelInfAboutUsers.Dock = DockStyle.Fill;
+            labelInfAboutUsers.Location = new Point(10, 10);
+            labelInfAboutUsers.Name = "labelInfAboutUsers";
+            labelInfAboutUsers.Size = new Size(664, 491);
+            labelInfAboutUsers.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(684, 561);
+            Controls.Add(panelBot);
+            Controls.Add(panelTop);
+            Name = "FormMain";
+            Text = "Пользователи";
+            Load += FormMain_Load;
+            panelTop.ResumeLayout(false);
+            panelBot.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelTop;
+        private Panel panelBot;
+        private Label labelUsers;
+        private Label labelInfAboutUsers;
+    }
+}
